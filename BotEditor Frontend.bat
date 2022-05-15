46a99abd5e250a7865969896a5bcf3d5729227cf
Raphael Carubbi Neto <rcarubbi@gmail.com>
2022-05-15 23:38:42 +0100

    fix samplesapi csproj file

--- BotEditor Frontend.bat
+++ BotEditor Frontend.bat
@@ -2,5 +2,5 @@
 
 start cmd /c "cd C:\Users\rcaru\source\repos\Carubbi.ChatbotStudio\carubbi-botconnector\ & yarn start"
 start cmd /c "cd C:\Users\rcaru\source\repos\Carubbi.ChatbotStudio\carubbi-boteditor\ & serve -s build -l 3000"
-start cmd /c "ngrok http 3979 --host-header=localhost:3979"
+start cmd /c "ngrok http 3980 --host-header=localhost:3980"
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --start-fullscreen http://localhost:3000 
